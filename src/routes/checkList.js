d7f18f4eff654596431146e0a8890987d6409636
Leandro-Pinho <leandroessencia@gmail.com>
2024-12-06 11:28:06 -0300

    criando o metodo post para inserir dados no banco

--- src/routes/checkList.js
+++ src/routes/checkList.js
@@ -2,34 +2,42 @@ const express = require("express");
 
 const router = express.Router();
 
-// trazer todas as tarefas 
+const Checklist = require("../models/checklist");
+
+// trazer todas as tarefas
 router.get("/", (req, res) => {
   console.log("Ola");
   res.send();
 });
 
 // adicionar tarefa
-router.post('/', (req, res)=> {
-    console.log(req.body)
-    res.status(200).send(req.body)
-})
+router.post("/", async (req, res) => {
+  let { name } = req.body;
+
+  try {
+    let checklist = await Checklist.create({ name });
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+});
 
 // trazer uma tarefa especifica pelo id
-router.get('/:id', (req, res)=> {
-    console.log(req.params.id)
-    res.send(`ID: ${req.params.id}`)
-})
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send(`ID: ${req.params.id}`);
+});
 
 // atualizar a tarefa
-router.put('/:id', (req, res)=> {
-    console.log(req.body)
-    res.send(`PUT ID: ${req.params.id}`)
-})
+router.put("/:id", (req, res) => {
+  console.log(req.body);
+  res.send(`PUT ID: ${req.params.id}`);
+});
 
-// deletar a tarefa 
-router.delete('/:id', (req, res)=> {
-    console.log(req.params.id)
-    res.send(`ID: ${req.params.id}`)
-})
+// deletar a tarefa
+router.delete("/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send(`ID: ${req.params.id}`);
+});
 
 module.exports = router;
