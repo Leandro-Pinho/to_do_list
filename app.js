2c5f240cce4e00051d5169c3f30da3fb13e81456
Leandro-Pinho <leandroessencia@gmail.com>
2024-12-06 13:21:02 -0300

    criando a pagina e rota com ejs

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const path = require("path");
+
 const checkListRouter = require("./src/routes/checkList");
+const rootRouter = require("./src/routes/index");
 require("./config/database");
 
 const app = express();
 app.use(express.json());
 
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
 app.use("/checkLists", checkListRouter);
+app.use("/", rootRouter);
 
 app.listen(3000, () => {
   console.log("Servidor rodando");
